e5abbc23d2ef8bd036d44bc9f46d390015922a40
shubhamj <shubham.cloudblitz@gmail.com>
2026-09-04 16:55:35 +0530

    fix

--- infra/backend/env/dev/var.tf
+++ infra/backend/env/dev/var.tf
@@ -47,7 +47,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.35"
 }
 
 variable "cluster_endpoint_public_access_cidrs" {
